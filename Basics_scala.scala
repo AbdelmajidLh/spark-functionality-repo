c3fd3047294065c7e99ac6fa0b1bcd4bd5712c23
EL HOU Abdelmajid <34235561+AbdelmajidLh@users.noreply.github.com>
2023-02-09 15:37:34 +0100

    Update Basics_scala.scala

--- Basics_scala.scala
+++ Basics_scala.scala
@@ -1,13 +1,16 @@
-// Databricks notebook source
-// DBTITLE 1,Les bases de scala
+// ############################################################
+// Les bases de scala
+// ############################################################
+
 // source : https://docs.scala-lang.org/tour/basics.html
 
-// COMMAND ----------
-
-// DBTITLE 1,Expressions
+// -----------------------------------------------------------------
+// Les Expressions 
+// -----------------------------------------------------------------
+// Vous pouvez faire des calculs dans scala
 1 + 1
-
-// COMMAND ----------
+2 * 6
+76 - 14
 
 // la fonction print est println()
 println(1) // 1
@@ -15,48 +18,41 @@ println(1 + 1) // 2
 println("Hello!") // Hello!
 println("Hello," + " world!") // Hello, world!
 
-// COMMAND ----------
-
-// DBTITLE 1,Values
+// -----------------------------------------------------------------
+// Les valeurs dans scala - Values
+// -----------------------------------------------------------------
 val x = 1 + 1
 println(x) // 2
-
-// COMMAND ----------
 
 // Les valeurs ne peuvent pas être réaffectées :
 x = 3 // Cela ne se compile pas.
 
-// COMMAND ----------
-
 // Le type d'une valeur peut être omis et déduit, ou il peut être explicitement indiqué :
 val x: Int = 1 + 1
 
-// COMMAND ----------
-
-// DBTITLE 1,Variables
+// -----------------------------------------------------------------
+// Les Variables dans scala 
+// -----------------------------------------------------------------
 // Les variables sont comme des valeurs, sauf que vous pouvez les réaffecter. Vous pouvez définir une variable avec le mot-clé var.
 var x = 1 + 1
 x = 3 // This compiles because "x" is declared with the "var" keyword.
 println(x * x) // 9
 
-// COMMAND ----------
-
 // Comme pour les valeurs, le type d'une variable peut être omis et déduit, ou bien il peut être explicitement indiqué :
 var x: Int = 1 + 1
 
-
-// COMMAND ----------
-
-// DBTITLE 1,Blocks
+// -----------------------------------------------------------------
+// Les blocs - Blocks
+// -----------------------------------------------------------------
 // Vous pouvez combiner des expressions en les entourant de {}. Nous appelons cela un bloc. Le résultat de la dernière expression du bloc est également le résultat de l'ensemble du bloc :
 println({
   val x = 1 + 1
   x + 1
 }) // 3
 
-// COMMAND ----------
-
-// DBTITLE 1,Functions
+// -----------------------------------------------------------------
+// Les fonction - Functions
+// -----------------------------------------------------------------
 // Les fonctions sont des expressions qui ont des paramètres et prennent des arguments.
 // Vous pouvez définir une fonction anonyme (c'est-à-dire une fonction qui n'a pas de nom) qui renvoie un nombre entier donné plus un :
 (x: Int) => x + 1
@@ -76,11 +72,11 @@ println(add(1, 2)) // 3
 val getTheAnswer = () => 42
 println(getTheAnswer()) // 42
 
-
-// COMMAND ----------
-
-// DBTITLE 1,Methods
-// Les méthodes ont un aspect et un comportement très similaires aux fonctions, mais il existe quelques différences essentielles entre elles. Les méthodes sont définies avec le mot-clé def. def est suivi d'un nom, d'une liste de paramètres, d'un type de retour et d'un corps :
+// -----------------------------------------------------------------
+// Les méthodes dans scala - Methods
+// -----------------------------------------------------------------
+// Les méthodes ont un aspect et un comportement très similaires aux fonctions, mais il existe quelques différences essentielles entre elles. 
+// Les méthodes sont définies avec le mot-clé def. def est suivi d'un nom, d'une liste de paramètres, d'un type de retour et d'un corps :
 def add(x: Int, y: Int): Int = x + y
 println(add(1, 2)) // 3
 
@@ -90,21 +86,18 @@ def addThenMultiply(x: Int, y: Int)(multiplier: Int): Int = (x + y) * multiplier
 println(addThenMultiply(1, 2)(3)) // 9
 
 
-// Or no parameter lists at all:
+// ou pas de paramètres du tout:
 def name: String = System.getProperty("user.name")
 println("Hello, " + name + "!")
 
-// COMMAND ----------
 
-// Methods can have multi-line expressions as well:
-// # scala 2 
+// Les méthodes peuvent avoir plusieurs lignes:
+// # scala 2 : la fonction return est optionnelle, les accolades sont obligatoires (vs scala 3)
 def getSquareString(input: Double): String = {
   val square = input * input
   return square.toString
 }
 println(getSquareString(2.5)) // 6.25
-
-// COMMAND ----------
 
 // # scala 3
 def getSquareString(input: Double): String =
@@ -113,7 +106,7 @@ def getSquareString(input: Double): String =
 
 println(getSquareString(2.5)) // 6.25
 
-// COMMAND ----------
-
-// DBTITLE 1,Classes
+// -----------------------------------------------------------------
+// Les Classes dans scala
+// -----------------------------------------------------------------
 
